5e446bec76511ed8670e677511105a9e05469cbc
Jonathan Gordon <jonathangordon@newrelic.com>
2017-10-02 16:12:59 -0700

    modified build to include a root dir

--- src/com/newrelic/metrics/publish/MacAgent.java
+++ src/com/newrelic/metrics/publish/MacAgent.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 public class MacAgent extends Agent {
 
     private static final String GUID = "com.newrelic.testing.fakePlugin";
-    private static final String VERSION = "1.0.0";
+    private static final String VERSION = "2.0.3";
     private final String name;
 
     public MacAgent(final String name) {
